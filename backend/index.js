cc0a230a4c8b6dba9537af0b60471d99c390484d
MUNSUJONG <101380117+MUNSUJONG@users.noreply.github.com>
2025-08-01 07:33:13 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,9 @@
-// backend/index.js
-
 const express = require('express');
+const cors = require('cors');
+const approveRouter = require('./approve');
+
 const app = express();
-
-// 반드시 Render가 감지할 수 있는 포트를 사용해야 함
-const PORT = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -16,6 +14,9 @@ app.get('/ping', (req, res) => {
   res.send('✅ Ping OK: Render 연결 정상');
 });
 
+app.use('/approve', approveRouter);
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ 서버 실행 중: http://localhost:${PORT}`);
 });
